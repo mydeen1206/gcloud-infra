b49673bb02abdb6d91a6cc08876d3611a9e7a93c
Mydeen <mydeen.devops@gmail.com>
2025-08-06 08:37:26 +0530

    change

--- compute/app1-webserver-install.sh
+++ compute/app1-webserver-install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt update -y
+sudo apt install iputils-ping -y
 sudo apt install -y telnet
 sudo apt install -y nginx
 sudo systemctl enable nginx
 sudo chmod -R 755 /var/www/html
 sudo mkdir -p /var/www/html/app1
 HOSTNAME=$(hostname)
-sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Google Cloud Platform - Demos</p> </body></html>" | sudo tee /var/www/html/app1/index.html
-sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Google Cloud Platform - Demos</p> </body></html>" | sudo tee /var/www/html/index.html
+sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p> Cloud DEMO </p> </body></html>" | sudo tee /var/www/html/app1/index.html
+sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Cloud Platform </p> </body></html>" | sudo tee /var/www/html/index.html
